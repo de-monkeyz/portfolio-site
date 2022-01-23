863e42c09b88f9b635af1ceca554830bcb1f2501
Ashley Blurton <a.k.blurton@gmail.com>
2022-01-23 12:19:29 +0000

    Theme toggle UI

--- src/UI/ThemeToggle.tsx
+++ src/UI/ThemeToggle.tsx
@@ -1,20 +1,103 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import { useThemeToggle } from "styles/theme/Context";
+import { motion, Transition } from "framer-motion";
 
-const ThemeToggle: React.FC = (props) => {
-  const [, toggleTheme] = useThemeToggle();
+import SunIcon from "assets/icons/carbon_sun.svg";
+import MoonIcon from "assets/icons/carbon_moon.svg";
+
+const AnimationConfig: Transition = {
+  type: "spring",
+  duration: 1,
+};
+
+const ThemeToggle: React.FC = () => {
+  const [theme, toggleTheme] = useThemeToggle();
+  const lastTheme = useRef(theme);
+  const [rotation, setRotation] = useState<number>(-1);
+
+  useEffect(() => {
+    if (!theme) {
+      return;
+    }
+
+    if (lastTheme.current !== theme) {
+      if (!lastTheme.current) {
+        setRotation(theme === "dark" ? 180 : 0);
+      } else {
+        setRotation((r) => (r += 180));
+      }
+    }
+    lastTheme.current = theme;
+  }, [theme]);
+
+  if (!theme || rotation < 0) {
+    return null;
+  }
+
   return (
     <ThemeToggleWrapper onClick={() => toggleTheme()}>
-      Toggle
+      <AnimationWrapper
+        initial={false}
+        animate={{
+          rotate: rotation,
+        }}
+        transition={AnimationConfig}
+      >
+        <Sun>
+          <SunIcon />
+        </Sun>
+        <Moon>
+          <MoonIcon />
+        </Moon>
+      </AnimationWrapper>
     </ThemeToggleWrapper>
   );
 };
 
+const Sun = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+
+  svg {
+    display: block;
+    width: 24px;
+    height: 24px;
+  }
+`;
+const Moon = styled(Sun)`
+  transform: rotate(180deg);
+`;
+const AnimationWrapper = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 200%;
+  transform-origin: center;
+`;
 const ThemeToggleWrapper = styled.button.attrs({ type: "button" })`
   position: absolute;
   top: 16px;
   left: 16px;
+  border: 0;
+  width: 44px;
+  height: 32px;
+  background: none;
+  /* box-shadow: var(--effect-lowShadow); */
+  /* border-radius: 3px; */
+  overflow: hidden;
+  /* background: var(--color-panel); */
+  transition: color 0.25s ease-in-out, border-bottom-color 0.25s ease-in-out;
+  cursor: pointer;
+  border-bottom: 2px solid rgba(0, 0, 0, 0);
+
+  &:hover {
+    border-bottom-color: var(--color-foreground);
+  }
 `;
 
 export default ThemeToggle;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 import { ThemeProvider } from "styles/theme/Context";
+import { MotionConfig } from "framer-motion";
 
 import Columns from "Layout/Columns";
 import ThemeToggle from "UI/ThemeToggle";
@@ -10,39 +11,45 @@ import Sidebar from "Sidebar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png "
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png "
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png "
-        />
-        <link rel="manifest" href="/site.webmanifest " />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5 " />
-        <meta name="msapplication-TileColor" content="#da532c " />
-        <meta name="theme-color" content="#ffffff " />
-      </Head>
-      <GlobalStyle />
-      <ThemeToggle />
-      <Columns columns="1fr 360px">
-        <main>
-          <Component {...pageProps} />
-        </main>
-        <Sidebar />
-      </Columns>
-    </ThemeProvider>
+    <MotionConfig reducedMotion="user">
+      <ThemeProvider>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png "
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png "
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png "
+          />
+          <link rel="manifest" href="/site.webmanifest " />
+          <link
+            rel="mask-icon"
+            href="/safari-pinned-tab.svg"
+            color="#5bbad5 "
+          />
+          <meta name="msapplication-TileColor" content="#da532c " />
+          <meta name="theme-color" content="#ffffff " />
+        </Head>
+        <GlobalStyle />
+        <ThemeToggle />
+        <Columns columns="1fr 360px">
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <Sidebar />
+        </Columns>
+      </ThemeProvider>
+    </MotionConfig>
   );
 }
 
