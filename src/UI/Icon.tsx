4fe3ea99357bd4451ac2defacd36d9a143dfdf20
Ashley Blurton <a.k.blurton@gmail.com>
2022-01-27 00:52:30 +0000

    Allow for fallback icon

--- src/UI/Icon.tsx
+++ src/UI/Icon.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { ComponentType } from "react";
 export interface IconProps {
-  name: string;
+  name?: string | null;
   className?: string;
+  fallback?: ComponentType<{ className?: string }>;
 }
 
-const Icon: React.FC<IconProps> = ({ name, className }) => {
+const Icon: React.FC<IconProps> = ({ name, className, fallback: Fallback }) => {
   let SVG = null;
   try {
-    SVG = require(`assets/icons/posts/${name}.svg`)?.default;
+    if (name) {
+      SVG = require(`assets/icons/posts/${name}.svg`)?.default;
+    }
   } catch (e) {}
   if (!SVG) {
+    if (Fallback) {
+      return <Fallback className={className} />;
+    }
     return null;
   }
   return <SVG className={className} />;
